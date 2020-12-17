b278a4297fe7382243d799cce4f9f472caef631c
Vladimir Khoroshilov <ddd333aaa@gmail.com>
2020-12-17 18:06:03 +0300

    refactor with class syntax

--- src/partials/calendar/calendar.js
+++ src/partials/calendar/calendar.js
@@ -1,172 +1,253 @@
-String.prototype.capitalize = function () {
-  return this.charAt(0).toUpperCase() + this.slice(1);
-};
 
-// let currentDate = new Date();
-// const calendarMonth = document.body.querySelector(".calendar__month");
-// const calendarTitle = document.body.querySelector(".calendar__title");
+const { 
+  calendarButtonPrev,
+  calendarButtonNext,
+  calendarTitle,
+  calendarMonth,
+  calendarDay,
+  calendarDayWithEvent,
+  calendarDayCurrent,
+  calendarDaySelected,
+  calendarWeekDay
+ } = {
+  calendarButtonPrev: 'calendar__button_prev',
+  calendarButtonNext: 'calendar__button_next',
+  calendarTitle: 'calendar__title',
+  calendarMonth: 'calendar__month',
+  calendarDay: 'calendar__day',
+  calendarDayWithEvent: 'calendar__day_with-event',
+  calendarDayCurrent: 'calendar__day_current',
+  calendarDaySelected: 'calendar__day_selected',
+  calendarWeekDay: 'calendar__week-day',
+ }
 
-// setCalendarTitleText(calendarTitle, currentDate);
-// addEmptyDays(calendarMonth, currentDate);
-// addDays(calendarMonth, currentDate);
+class Calendar {
+  constructor(node) {
+
+    this._node = node;
+    this._titleNode = this._node.querySelector(`.${calendarTitle}`);
+    this._date = new Date();
+    this._cachedMonths = new Map();
+    this._cachedDayNodes = new Map();
+    this._cachedEmptyDayNodes = new Map();
+    this._selectedDay = null;
+    this._monthNode = this._node.querySelector(`.${calendarMonth}`);
+    this._prevButton = this._node.querySelector(`.${calendarButtonPrev}`);
+    this._nextButton = this._node.querySelector(`.${calendarButtonNext}`);
 
 
-// let lastMonth = new Date(currentDate);
-// lastMonth.setDate(0);
+    this.fillCalendar();
+    this.addDateSwitch();
 
+    this._monthNode.addEventListener('click', this._daySelectorHandler.bind(this))
 
+  }
 
-// function getLocalDay(date) {
+  //делаем запрос на сервер и кешируем его
+  _getMonth(date) { 
 
-//   let day = date.getDay();
+    let firstDayOfMonth = new Date(date);
+    firstDayOfMonth.setDate(1);
 
-//   if (day == 0) {
-//     day = 7;
-//   }
+    let lastDayOfMonth = new Date(date);
+    lastDayOfMonth.setDate(Calendar.daysInMonth(lastDayOfMonth));
+    
+    let key = Calendar.getMonthAndYear(firstDayOfMonth);
 
-//   return day;
+    if (!this._cachedMonths.has(key)) {
 
-// }
-
-// function setCalendarTitleText(node, date) {
-
-//   let calendarTitleText =
-
-//     date.toLocaleDateString("ru-RU", { month: "long" }).capitalize() +
-//     " " +
-//     date.toLocaleDateString("ru-RU", { year: "numeric" });
-
-//   node.innerText = calendarTitleText;
-
-// }
-
-// function addEmptyDays(node, date) { // добавляет пустые элементы если месяц начинается не с понедельника
-
-//   let firstDay = new Date(date);
-//   firstDay.setDate(1);
-
-//   for (let i = 1; i < getLocalDay(firstDay); i++) {
-
-//     let day = document.createElement("p");
-  
-//     day.className = "calendar__day";
-  
-//     node.append(day);
-  
-//   }
-
-// }
-
-// function addDays(node, date) {
-
-//   let daysInMonth = 33 - new Date(date.getFullYear(), date.getMonth(), 33).getDate();
-
-//   for (let i = 1; i <= daysInMonth; i++) {
-
-//     let day = document.createElement("button");
-  
-//     day.className = "calendar__day";
-//     day.innerText = i;
-  
-//     node.append(day);
-  
-//   }
-
-// }
-
-// function deleteDays(node) {
-
-//   let nodesToDelete = [];
-
-//   node.childNodes.forEach((item) => {
-
-//     if (!item.classList.contains('calendar__week-day')) {
-//       nodesToDelete.push(item);
-//     }
-
-//   });
-
-//   nodesToDelete.forEach((item) => item.remove());
-// }
-
-// function changeMonth(date) {
-//   setCalendarTitleText(calendarTitle, date);
-//   deleteDays(calendarMonth);
-//   addEmptyDays()
-// }
-
-function Calendar(node) {
-
-  this.self = node;
-  this.date = new Date();
-  this.title = node.querySelector(".calendar__title");
-  this.month = node.querySelector(".calendar__month");
-
-  this.getLocalDay = function() {
-
-    let day = this.date.getDay();
-
-    if (day == 0) {
-      day = 7;
+      let response = getDaysFromServer(firstDayOfMonth, lastDayOfMonth);
+      this._cachedMonths.set(key, response)
     }
-  
-    return day;
-  };
 
-  this.setCalendarTitleText = function() {
-    let calendarTitleText =
+    this._currentDays = this._cachedMonths.get(key);
 
-    this.date.toLocaleDateString("ru-RU", { month: "long" }).capitalize() +
-    " " +
-    this.date.toLocaleDateString("ru-RU", { year: "numeric" });
+  }
 
-    this.title.innerText = calendarTitleText;
-  };
+  //после того как данные будут получены с сервера здесь нужно дням с событиями назначить класс
+  _setDaysWithEvents() {
+      
+  }
 
-  this.addEmptyDays = function() {
+  _setCalendarTitleText() {
+    let calendarTitleText = capitalize(this._date.toLocaleDateString("ru-RU", { month: "long" })) +
+      " " +
+      this._date.toLocaleDateString("ru-RU", { year: "numeric" });
 
-    let firstDay = new Date(this.date);
-    firstDay.setDate(1);
+    this._titleNode.innerText = calendarTitleText;
+  }
 
-    for (let i = 1; i < this.getLocalDay(); i++) {
+  // добавляем дни в DOM
+  _addDays() {  
 
-      let day = document.createElement("p");
-    
-      day.className = "calendar__day";
-    
-      this.month.append(day);
-    
+    let key = Calendar.getMonthAndYear(this._date);
+
+    if (!this._cachedDayNodes.has(key)) { // если для текущего месяца не было создано элементов, то создаем и кешируем
+      let days = []
+      let currentDay = (new Date(this._date).getMonth() == new Date().getMonth()) ? this._date.getDate(): null;
+
+      for (let i = 1; i <= Calendar.daysInMonth(this._date); i++) {
+
+        let day = document.createElement("button");
+
+        day.classList.add(calendarDay);
+
+        if (this._currentDays[i - 1].haveEvent) {
+          day.classList.add(calendarDayWithEvent);
+        }
+
+        if (i == currentDay) {
+          day.classList.add(calendarDayCurrent);
+        }
+
+        day.innerText = i;
+
+        days.push(day);
+      }
+
+      this._cachedDayNodes.set(key, days);
     }
-  };
 
-  this.addDays = function() {
-    let daysInMonth = 33 - new Date(this.date.getFullYear(), this.date.getMonth(), 33).getDate();
-
-    for (let i = 1; i <= daysInMonth; i++) {
-
-      let day = document.createElement("button");
+    this._cachedDayNodes.get(key).forEach((item) => {
+      this._monthNode.append(item);
+    })
     
-      day.className = "calendar__day";
-      day.innerText = i;
-    
-      this.month.append(day);
+  }
+
+  // добавляем в DOM пустые элементы чтобы дни месяца соответсвовали дням недели
+  _addEmptyDays() { 
+
+    let key = Calendar.getMonthAndYear(this._date);
+
+    let cache = this._cachedEmptyDayNodes;
+
+    if (!this._cachedEmptyDayNodes.has(key)) { // если для текущего месяца нет пустых элементов, то создаем и кешируем
+
+      let firstDay = new Date(this._date);
+      firstDay.setDate(1);
+
+      let emptyDays = [];
+
+      for (let i = 1; i < Calendar.getLocalDay(firstDay); i++) {
+
+        let day = document.createElement("p");
+
+        day.className = "calendar__day";
+
+        emptyDays.push(day);
+
+      }
+
+      this._cachedEmptyDayNodes.set(key, emptyDays);
+      
     }
-  };
 
-  this.removeDays = function() {
+    this._cachedEmptyDayNodes.get(key).forEach((item) => {
+      this._monthNode.append(item);
+    })
+    
+  }
+
+  // удаляем из DOM все кроме названия дней недели
+  _removeDays() { 
+
     let nodesToDelete = [];
 
-    node.childNodes.forEach((item) => {
+    this._monthNode.childNodes.forEach((item) => {
 
-      if (!item.classList.contains('calendar__week-day')) {
+      if (!item.classList.contains(calendarWeekDay)) {
         nodesToDelete.push(item);
       }
 
     });
 
     nodesToDelete.forEach((item) => item.remove());
+
   }
 
-  this.addEmptyDays();
-  this.addDays();
+  // если элемент с событием, то назначаем ему класс
+  _daySelectorHandler(event) { 
+
+    if (!event.target.classList.contains(calendarDayWithEvent)) {return}
+
+    if (event.target != this._selectedDay && this._selectedDay != null) {this._selectedDay.classList.remove(calendarDaySelected)}
+
+    this._selectedDay = event.target;
+    event.target.classList.add(calendarDaySelected);
+
+    // здесь нужно сделать запрос на сервер и обновить элементы event
+
+  }
+
+  fillCalendar() {
+    this._getMonth(this._date);
+    this._setCalendarTitleText();
+    this._addEmptyDays();
+    this._addDays();
+    this._setDaysWithEvents()
+
+  }
+
+  addDateSwitch() { 
+    this._prevButton.addEventListener('click', () => {
+      this._date.setDate(0);
+      this._setDaysWithEvents(this._date);
+      this._removeDays();
+      this.fillCalendar();
+    });
+
+    this._nextButton.addEventListener('click', () => {
+      this._date.setDate(Calendar.daysInMonth(this._date) + 1);
+      this._setDaysWithEvents(this._date);
+      this._removeDays();
+      this.fillCalendar();
+    })
+  }
+
+  static daysInMonth(date) {
+    return 33 - new Date(date.getFullYear(), date.getMonth(), 33).getDate();
+  }
+
+  static getLocalDay(date) {
+
+    let day = date.getDay();
+
+    if (day == 0) {
+      day = 7;
+    }
+
+    return day;
+
+  }
+
+  static getMonthAndYear(date) {
+    let dateString = date.toDateString().split(' ');
+    return `${dateString[1]} ${dateString[3]}`
+  }
+
 }
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+//
+function getDaysFromServer(from, to) {
+  let days = [];
+  for (let i = 1; i <= Calendar.daysInMonth(from); i++) {
+    let day = new Date(from);
+    day.setDate(i);
+    days.push({
+      date: new Date(day),
+      haveEvent: false,
+    })
+  }
+  days[Math.round(Math.random() * (days.length - 1))].haveEvent = true;
+  days[Math.round(Math.random() * (days.length - 1))].haveEvent = true;
+  days[Math.round(Math.random() * (days.length - 1))].haveEvent = true;
+  days[Math.round(Math.random() * (days.length - 1))].haveEvent = true;
+  return days;
+}
+
+
+let calendar = new Calendar(document.querySelector('.calendar'))
